fa7c8882bb1dedcd71743850facfad36fd38d54c
Mateo González <minidrw@gmail.com>
2024-09-30 18:03:17 -0300

    chore: change small breakpoint to 420px

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     content: ["./index.html", "./src/**/*.{html,jsx}" ], 
-    theme: { extend: {}, }, 
+    theme: { extend: {
+        screens: {
+            'sm': '425px',
+            // => @media (min-width: 640px) { ... }
+      
+            'md': '768px',
+            // => @media (min-width: 768px) { ... }
+      
+            'lg': '1024px',
+            // => @media (min-width: 1024px) { ... }
+      
+            'xl': '1280px',
+            // => @media (min-width: 1280px) { ... }
+      
+            '2xl': '1536px',
+            // => @media (min-width: 1536px) { ... }
+        }
+    }, }, 
     safelist: [ { pattern: /(bg|text)-.*-./, } ], 
     darkMode: "class", plugins: [], 
 }
