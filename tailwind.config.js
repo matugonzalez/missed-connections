925961b22c554db693a820ca08a36d39e9c8fce9
Mateo González <minidrw@gmail.com>
2024-10-01 00:00:46 -0300

    chore: add custom animations to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,43 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     content: ["./index.html", "./src/**/*.{html,jsx}" ], 
-    theme: { extend: {
-        screens: {
-            'sm': '425px',
-            // => @media (min-width: 640px) { ... }
-      
-            'md': '768px',
-            // => @media (min-width: 768px) { ... }
-      
-            'lg': '1024px',
-            // => @media (min-width: 1024px) { ... }
-      
-            'xl': '1280px',
-            // => @media (min-width: 1280px) { ... }
-      
-            '2xl': '1536px',
-            // => @media (min-width: 1536px) { ... }
-        }
-    }, }, 
+    theme: { 
+        extend: {
+            screens: {
+                'sm': '425px',
+                // => @media (min-width: 640px) { ... }
+        
+                'md': '768px',
+                // => @media (min-width: 768px) { ... }
+        
+                'lg': '1024px',
+                // => @media (min-width: 1024px) { ... }
+        
+                'xl': '1280px',
+                // => @media (min-width: 1280px) { ... }
+        
+                '2xl': '1536px',
+                // => @media (min-width: 1536px) { ... }
+            },
+            keyframes: {
+                wiggle: {
+                  '0%, 100%': { transform: 'rotate(-3deg)' },
+                  '50%': { transform: 'rotate(3deg)' },
+                },
+                shake: {
+                    '0%, 100%': {transform: 'translateX(0)' },
+                    '25%' : {transform: 'translateX(-5px)' }, 
+                    '50%' : {transform: 'translateX(5px)' },
+                    '75%' : {transform: 'translateX(-5px)' },
+                },
+            },
+            animation: {
+                wiggle: 'wiggle 1s ease-in-out 1s',
+                shake: 'shake 0.5s ease'
+            }
+        }, 
+    }, 
     safelist: [ { pattern: /(bg|text)-.*-./, } ], 
-    darkMode: "class", plugins: [], 
+    darkMode: "class", 
+    plugins: [], 
 }
